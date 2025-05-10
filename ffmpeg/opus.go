737a053557d6c50329710cf9cdd7efd7c0e3ba1b
fluffy-melli <fluffy-melli@users.noreply.github.com>
2025-05-10 14:18:41 +0900

    Add function to stream Opus conversion with chunked output

--- ffmpeg/opus.go
+++ ffmpeg/opus.go
@@ -2,6 +2,7 @@ package ffmpeg
 
 import (
 	"bytes"
+	"io"
 	"os/exec"
 )
 
@@ -27,6 +28,57 @@ func ToOpus(opusData []byte, callback func([]byte)) error {
 	}
 
 	callback(buffer.Bytes())
+
+	return nil
+}
+
+func ToOpusStream(opusData []byte, callback func([]byte), bufferSize int) error {
+	cmd := exec.Command("ffmpeg",
+		"-i", "pipe:0",
+		"-c:a", "libopus",
+		"-f", "opus",
+		"pipe:1",
+	)
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	go func() {
+		defer stdin.Close()
+		_, _ = stdin.Write(opusData)
+	}()
+
+	buf := make([]byte, bufferSize)
+	for {
+		n, err := stdout.Read(buf)
+		if n > 0 {
+			callback(buf[:n])
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
 
 	return nil
 }
